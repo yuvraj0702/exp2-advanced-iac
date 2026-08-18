bebbf9ad57f3bd24bed69f7649bfc11b43be4923
Yuvraj <your-email@example.com>
2026-08-18 21:26:38 +0530

    Revert Step 9 test SSH ingress

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "web" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.admin_cidr]
   }
 
   egress {
