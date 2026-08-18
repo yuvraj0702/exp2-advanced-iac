a89b388287cee9098c8514360c4e0573d351c449
Yuvraj <your-email@example.com>
2026-08-18 21:24:07 +0530

    Test: widen SSH ingress for Step 9

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "web" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.admin_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
